90f1c7b19e3bc4fb8699bd2a3a9556630c54b67b
ThomasHolmes00 <thomas.scott.holmes00@gmail.com>
2022-10-24 20:45:52 -0600

    Javascript updated file

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Keep your passwords secure by using this webpage. Enjoy!
 <br></br>
 
 ## Screenshot
-![Screenshot of what the webpage looks like](./img/PasswordGenerator.png)
+![Screenshot of what the webpage looks like](./img/PasswordGenerator..png)
 
 <br></br>
 
--- js/script.js
+++ js/script.js
@@ -1,30 +1,33 @@
-// Assignment Code 
-// Event listener to generate button
-var generateBtn = document.querySelector("#generate").addEventListener("click", writePassword);
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-// Write Password Function
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
-}
-function UserInput(ps) {
-  document.getElementById("password").textContent = ps;
-}
-
-// Password Functions
-function generatePassword() {
-  var special = ["!, %, &, *, +, -, ., /, <, >"];
-  var number = ["0, 1, 2, 3, 4, 5, 6, 7, 8, 9"];
-  var lowerCase = ["a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"];
-  var upperCase = ["A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z"];
-  var lengthValue;
-  var confirmSpecial;
-  var confirmNumber;
-  var confirmUpperCase;
-  var confirmLowerCase;
-  var options;
-
-
 
 }
+
+function generatePassword(length = 8) {
+  let password = '';
+  const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()";
+  let passwordLength = length;
+
+  const array = new Uint32Array(length);
+  window.crypto.getRandomValues(array);
+
+  for (i=0; i<length; i++) {
+    password += chars[array[i] % chars.length];
+  }
+  console.log(password);
+  return password;
+}
+
+generatePassword(5);
+generatePassword(8);
+generatePassword(10);
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
